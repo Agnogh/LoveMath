c85c684f48d9df580383fe12adfdcefa75891cbd
Agnogh <agnogh_orion@yahoo.com>
2025-04-19 22:27:33 +0200

    trying to get alert message, but failing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
+
+
+
+/* Good idea to wait for DOM to finish loading before starting to run the code as it might trying to target elements that havent loaded yet and dont exist */
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");  /* we are tagging by tag name that is button and we have 5 in total */ 
+ /*   for (let i = 0; i < buttons.length; i++) -> this is less modern syntax) */
+    for (let button of buttons) { /* goes through our button array and return each element in array which will be stored in that variable button on each iteration */
+        button.addEventListener("click", function() {   /* button represents individual button element  */ 
+            if (this.getAttribute("data_type") === "submit") { /* "this" refers to the button that was just clicked. Checks the attribute of the data type to see the value and if it is EQUAL to submit you are getting Alert message*/
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data_type");
+                alert(`You clicked ${gameType}`); 
+            }
+        })
+    }
+})
+
+
+
 function runGame() {
 
 }
